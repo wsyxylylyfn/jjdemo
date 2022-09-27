502aa77bfa76fceb523300e12bf5b81766f035cd
itws <779701874@qq.com>
2022-09-27 22:35:48 +0800

    家家旅途

---
+++ src/components/tab-bar/tab-bar_03.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path" icon="home-o">
+          <span>{{ item.text }}</span>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup>
+
+import tabbarData from "@/assets/data/tabbar.js"
+import { getAssetURL } from "@/utils/load_assets.js"
+import { ref } from "vue";
+
+const currentIndex = ref(0)
+
+</script>
+
+<style lang="less" scoped>
+.tab-bar {
+ 
+
+ 
+ 
+  :deep(.van-tabbar-item__icon) {
+    font-size: 50px;
+  }
+
+  img {
+    height: 28px;
+  }
+}
+
+</style>
---
+++ src/utils/load_assets.js
@@ -0,0 +1,6 @@
+export const getAssetURL = (image) => {
+  
+  return new URL(`../assets/img/${image}`, import.meta.url).href
+}
+
+
